71ac1d0a93b0ca710097694515db5f1a1ea1c977
Renato Lacerda <renato.ac.lacerda@gmail.com>
2023-07-04 11:35:14 -0300

    styles: reset, utils e variaveis iniciais

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,17 @@
 <template>
-
+  <div class="container">
+    <header>
+      <h1>Skybook</h1>
+      <a href="#"> Test link</a>
+    </header>
+    <main>
+      <router-view></router-view>
+    </main>
+  </div>
 </template>
+
+<style scoped lang="scss">
+h1 {
+  font-size: 3rem;
+}
+</style>
