3b954470074b17c5dd0fd8b4920ccc15b9f30576
Renato Lacerda <renato.ac.lacerda@gmail.com>
2023-07-04 14:20:29 -0300

    styles: estilo inicial para pagina home

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,6 @@ const contactsStore = useContactsStore();
 <template>
   <header>
     <h1>Skybook</h1>
-    <a href="#"> Test link</a>
   </header>
   <main>
     <ul class="contact-list">
@@ -22,6 +21,42 @@ const contactsStore = useContactsStore();
 
 <style scoped lang="scss">
 h1 {
-  font-size: 3rem;
+  font-size: var(--font-xl);
+}
+
+h2 {
+  font-size: var(--font-lg);
+  padding-block: 1em;
+  text-align: center;
+  border-radius: 0.5rem;
+  background-color: var(--bg-secondary);
+  margin-bottom: 1em;
+}
+
+.contact-list {
+  list-style: none;
+}
+
+.contact-info {
+  font-size: var(--font-md);
+  padding: 0.5rem 1rem;
+  border-radius: 0.5rem;
+  background-color: var(--bg-secondary);
+  line-height: 1.4;
+  cursor: pointer;
+
+  &:not(:last-of-type) {
+    margin-bottom: 1em;
+  }
+
+  &:hover {
+    background-color: var(--bg-accent);
+  }
+}
+
+.contact-phone,
+.contact-email {
+  color: var(--fg-secondary);
+  font-size: var(--font-sm);
 }
 </style>
