28b605a2961005510d0d9a61e3888d927eb735a2
Renato Lacerda <renato.ac.lacerda@gmail.com>
2023-07-05 10:01:14 -0300

    fix: build para browsers que possuem top level await

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,11 @@ import icons from "unplugin-icons/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), icons()],
+  // build para browsers que possuem top level await
+  // https://caniuse.com/mdn-javascript_operators_await_top_level
+  esbuild: {
+    supported: {
+      "top-level-await": true,
+    },
+  },
 });
